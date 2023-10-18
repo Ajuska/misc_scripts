0aa22786a408af1772f573c6d942889fdb9077ad
Andrea Vicarova <a.kopecna@seznam.cz>
2023-10-18 22:52:19 +0200

    feat: add number to audifile title tag so sorting works properly

--- download_scripts/audiobook.py
+++ download_scripts/audiobook.py
@@ -1,13 +1,15 @@
+# Python 3.9.16 | Creator: Andrea Vicarova
 # Download script for mp3 audiofiles
 # You need to change site url and folder name each time
 # (site url differes a lot and there is no pattern to automate it)
 import os
 import requests
+import eyed3
 
 
-site_url = 'https://ipaudio.club/wp-content/uploads/APP/Algorithms%20to%20Live%20By/'
-folder_name = "BRIAN CHRISTIAN – ALGORITHMS TO LIVE BY AUDIOBOOK".title()
-
+site_url = ''
+folder_name = "".title()
+book_name = folder_name.split('–')[-1].strip()
 
 if not os.path.exists(folder_name):
     os.makedirs(folder_name)
@@ -24,6 +26,12 @@ while number:
 
         with open(file_path, "wb") as file:
             file.write(response.content)
+
+        # add number to a file title in case it's missing
+        audiofile = eyed3.load(file_path)
+        audiofile.tag.title = f"{file_number} | {book_name}"
+        audiofile.tag.save()
+
         number +=1
         print(f"Downloaded {file_name}")
     elif response.status_code == 404:
